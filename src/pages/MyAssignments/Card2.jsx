797217379815a1dd64bfc4596e34c883f8d14712
Mabu1 <umahtab65@gmail.com>
2023-11-08 20:32:02 +0600

    my page added

--- src/pages/MyAssignments/Card2.jsx
+++ src/pages/MyAssignments/Card2.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import { AiOutlineFilePdf } from 'react-icons/ai';
 
 const Card2 = ({ assignment }) => {
-  const { title, pdfLink, quickNote, status, marks,name } = assignment;
-const sta="completed";
+  const { title, pdfLink, quickNote, status, marks, name } = assignment;
+  const sta = "completed";
   return (
     <div className="w-[340px] mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-      <div className='flex justify-center'>
-      <h2 className="text-blue-600 text-2xl font-bold my-2" >{name}</h2>
 
-      </div>
       {/* PDF Icon */}
       <div className="text-center mt-4">
         <AiOutlineFilePdf className="w-16 h-16 mx-auto text-red-500" />
       </div>
-        {/* "View" Button */}
-        <div className="text-center">
+      {/* "View" Button */}
+      <div className="text-center">
         <a
           href={pdfLink} // Set the PDF link as the href
           target="_blank"
@@ -25,15 +22,26 @@ const sta="completed";
           View
         </a>
       </div>
-      <div className="p-4">
+
+      <div className='flex justify-center mt-4'>
+        <h2 className="text-blue-600 text-2xl font-bold my-2" >{name}</h2>
+
+      </div>
+      <div className="p-4 mt-4">
         <h2 className="text-4xl font-semibold my-2">{title}</h2>
         {status === sta ? (
-          <p className="text-red-600 text-3xl">You Scored: {marks}</p>
+          <p className="text-red-600 text-3xl my-4">You Scored: {marks}</p>
         ) : (
-          <p className="text-red-600 text-3xl">{status}</p>
+          <button
+            className="bg-red-500 text-white py-1 text-xl px-2 rounded my-4"
+
+          >
+            {status}
+          </button>
+
         )}
       </div>
-    
+
     </div>
   );
 };
