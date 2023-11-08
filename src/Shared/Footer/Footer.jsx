65f60b93ee965219063b518a39d7a693cb7cdda6
Mabu1 <umahtab65@gmail.com>
2023-11-08 21:11:39 +0600

    footer added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 
-import {  MdEmail } from 'react-icons/md';
+import { MdEmail } from 'react-icons/md';
 import { BsTelephoneFill } from 'react-icons/bs';
 import { FaFacebookSquare, FaYoutube } from 'react-icons/fa';
 import { RiInstagramFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    
+
 
     return (
         <div>
@@ -19,7 +19,7 @@ const Footer = () => {
                             </div>
                             <div>
                                 <ul className='mt-[-30px] flex flex-col gap-2'>
-                                   
+
                                     <li className='flex gap-2 items-center'><BsTelephoneFill className='text-[#2cae74] text-2xl' /><p>+8 (800) 238 9997 (admin)</p></li>
                                     <li className='flex gap-2 items- center'><MdEmail className='text-[#2cae74] text-2xl' /><p>estudy77@gmail.com</p></li>
                                 </ul>
@@ -27,12 +27,12 @@ const Footer = () => {
                         </div>
                         <div>
                             <h2 className='text-4xl mt-5 mb-8 font-semibold text-[#7207ff] '>Pages</h2>
-                           <li><Link className="text-blue-300 text-lg font-bold" to='/assignments'>Assignments</Link></li>
-                           <li><Link className="text-blue-300 text-lg font-bold" to='/create'>Create Assignments</Link></li>
-                           <li><Link className="text-blue-300 text-lg font-bold" to='/submissions'>Submissions</Link></li>
-                           <li><Link className="text-blue-300 text-lg font-bold" to='/myassignment'>My Assignments</Link></li>
-                           <li><Link className="text-blue-300 text-lg font-bold" to='/login'>login</Link></li>
-                           <li><Link className="text-blue-300 text-lg font-bold" to='/register'>Register</Link></li>
+                            <li><Link className="text-blue-300 text-lg font-bold" to='/assignments'>Assignments</Link></li>
+                            <li><Link className="text-blue-300 text-lg font-bold" to='/create'>Create Assignments</Link></li>
+                            <li><Link className="text-blue-300 text-lg font-bold" to='/submissions'>Submissions</Link></li>
+                            
+                            <li><Link className="text-blue-300 text-lg font-bold" to='/login'>login</Link></li>
+                            <li><Link className="text-blue-300 text-lg font-bold" to='/register'>Register</Link></li>
                         </div>
                         <div>
                             <h2 className='font-semibold text-4xl  mt-5 mb-3 text-[#3d07ff]'>Social Links</h2>
@@ -43,7 +43,7 @@ const Footer = () => {
                                 <FaYoutube className='text-[#dd1d1d] text-4xl' />
                             </div>
                         </div>
-                      
+
                     </div>
                 </div>
             </div>
